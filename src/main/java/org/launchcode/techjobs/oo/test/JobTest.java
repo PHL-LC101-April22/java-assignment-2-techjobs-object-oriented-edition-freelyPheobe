1be86dc8997711efb53295a0073b82e6e9c5d2ef
freelyPheobe <104399179+freelyPheobe@users.noreply.github.com>
2022-09-15 19:39:46 -0400

    testsettingJobId completes

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,34 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+import org.launchcode.techjobs.oo.*;
+
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void emptyTest() {
+        assertEquals(10,10,.001);
+    }
 
+    @Test
+    public void testSettingJobId() {
+       Job object = new Job();
+       Job object2 = new Job();
+        assertNotEquals(object.getId(), object2.getId());
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+    Job object3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue();
+
+    }
 }
