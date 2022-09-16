4b241024a486d6eafe419d8b83eb730165a33968
freelyPheobe <104399179+freelyPheobe@users.noreply.github.com>
2022-09-15 20:38:14 -0400

    assertTrue and assertEquals 10 tests completed/passed.

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.oo.test;
 
+import com.sun.jdi.Value;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -28,7 +29,16 @@ public class JobTest {
     @Test
     public void testJobConstructorSetsAllFields() {
     Job object3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        assertTrue();
+        assertTrue(object3.getName() instanceof String);
+        assertEquals("Product tester", object3.getName());
+        assertTrue(object3.getEmployer() instanceof Employer);
+        assertEquals("ACME", object3.getEmployer().getValue());
+        assertTrue(object3.getLocation() instanceof Location);
+        assertEquals("Desert", object3.getLocation().getValue());
+        assertTrue(object3.getPositionType() instanceof PositionType);
+        assertEquals("Quality control", object3.getPositionType().getValue());
+        assertTrue(object3.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Persistence", object3.getCoreCompetency().getValue());
 
     }
 }
